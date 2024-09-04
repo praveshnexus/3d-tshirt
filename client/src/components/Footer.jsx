349e6b94aaf3afb22e89ff4aaeb8d3e277614093
Pravesh <ypravesh0007@gmail.com>
2024-09-04 23:23:47 +0530

    first commit

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     return (
       <>
           <motion.footer className="text-gray-800 w-fit mx-auto pb-1" {...slideAnimation('up')}>
-              created with <span className="text-red-600">❤️</span> by <a href="https://davegizmo.vercel.app" target="_blank" className="underline">davegizmo</a>
+              created with <span className="text-red-600">❤️</span> by Pravesh.
           </motion.footer>
       </>
     )
